8d36265c8d51d2f72ce0b54dec887fd34b227bf1
shipra <shipra_visualstudio>
2022-01-17 02:58:34 -0800

    Updated Default data in MockDataStore.cs

--- project_mine/Mine/Mine/Services/MockDataStore.cs
+++ project_mine/Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Bunny Shell", Description="Just a funny name of Item.", Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Beautiful people", Description="World full of neautiful people.", Value=5 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Mobile Dev Class", Description="This is a Mobile Dev class.", Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "What to add", Description="Confusion???", Value=6 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sample Data", Description="This is a sample data.", Value=7 }
             };
         }
 
