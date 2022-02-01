6c8fae022c69fb1a425013a1601b51fe591f2e84
Fnu Shipra <fshipra@seattleu.edu>
2022-02-01 13:18:47 -0800

    Added Delete to DatabaseService.cs

--- project_mine/Mine/Mine/Services/DatabaseService.cs
+++ project_mine/Mine/Mine/Services/DatabaseService.cs
@@ -79,9 +79,26 @@ namespace Mine.Services
             return true;
         }
 
-        public Task<bool> DeleteAsync(string id)
+        /// <summary>
+        /// Code to delete the data from database using the ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            var data = await ReadAsync(id);
+            if (data == null)
+            {
+                return false;
+            }
+
+            var result = await Database.DeleteAsync(data);
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
