815a6a3c9f39cb9f8e334a9f456a8437d2b8fd91
Shipra Shipra <fshipra@seattleu.edu>
2022-02-01 10:23:42 -0800

    Changed DependencyService to use DatabaseService in App.xaml.cs

--- project_mine/Mine/Mine/App.xaml.cs
+++ project_mine/Mine/Mine/App.xaml.cs
@@ -8,12 +8,15 @@ namespace Mine
 {
     public partial class App : Application
     {
-
+        /// <summary>
+        /// Changed DependencyService to use DatabaseService
+        /// </summary>
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
+            //DependencyService.Register<MockDataStore>();
+            DependencyService.Register<DatabaseService>();
             MainPage = new MainPage();
         }
 
