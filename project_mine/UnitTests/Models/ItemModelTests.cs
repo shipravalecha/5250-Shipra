487ad237ac6361d8698b452fd1b60b6842ca5321
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-08 00:22:28 -0800

    Added ItemModelTests.cs to the project

---
+++ project_mine/UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert
+            Assert.Pass();
+        }
+    }
+}
