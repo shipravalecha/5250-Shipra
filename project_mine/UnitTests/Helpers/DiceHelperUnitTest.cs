fb95e217e916da49887426cb5e92a181af7a47b4
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-08 01:14:30 -0800

    Added RollDice_Valid_Roll_1_Dice_6 to DiceHelperUnitTest.cs

--- project_mine/UnitTests/Helpers/DiceHelperUnitTest.cs
+++ project_mine/UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -14,11 +14,28 @@ namespace UnitTests.Helpers
         public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
         {
             // Arrange
+
             // Act
             var result = DiceHelper.RollDice(0, 1);
+
             // Reset
+
             // Assert 
             Assert.AreEqual(0, result);
+        }
+
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(true, result >= 1);
+            Assert.AreEqual(true, result <= 6);
         }
     }
 }
